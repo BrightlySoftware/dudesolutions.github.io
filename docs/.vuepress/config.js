ac9f3e09d0e0cf60d88215114cee40a51359f6e8
BrandonEMiller <brandonevanmiller@gmail.com>
2018-07-25 10:16:34 -0400

    Stubbing out pages for Open Source and Front End development

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { 
+          text: 'Development', 
+          items: [
+            { text: 'Front End', link: '/FrontEnd' }
+          ] 
+        },
+        { text: 'Open Source', link: '/OpenSource' },
+        { text: 'External', link: 'https://google.com' },
+      ]
+    }
+  }
